3c896871d57f9d78f5f6bc2840179ebf853490c4
Viktor Edström <veds@kth.se>
2017-11-21 14:50:01 +0100

    readme, docker-swarm now doesn't expose backend service data

--- web-service/app.py
+++ web-service/app.py
@@ -2,13 +2,14 @@ from flask import Flask
 import requests
 
 app = Flask(__name__)
-#
+
 @app.route("/")
 def index():
     r = requests.get("http://magic-backend-service:7331/")
     return r.text
+
 def main():
-    app.run(host="0.0.0.0", port=1337)
+    app.run(host="0.0.0.0", port=80)
 
 if __name__ == "__main__":
     main()
